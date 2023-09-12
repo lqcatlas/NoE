faeea81523be075e408a30d9146f58dda594d0ba
lqcatlas <lqc.atlas@gmail.com>
2023-09-12 08:52:24 +0800

    Selector theme swing VFX

--- Assets/Scripts/VFX/ObjSwinging.cs
+++ Assets/Scripts/VFX/ObjSwinging.cs
@@ -10,7 +10,12 @@ public class ObjSwinging : MonoBehaviour
 
     private void OnEnable()
     {
+        bool startReversed = Random.Range(0, 2) == 1;
+
         transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+        transform.DORotate(new Vector3(0f,0f,Random.Range(SWINGING_RANGE.x, SWINGING_RANGE.y) * (startReversed ? 1 : -1)), Random.Range(SWINGING_LOOP_TIME.x, SWINGING_LOOP_TIME.y))
+            .SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine)
+            .SetRelative(true);
     }
     private void OnDisable()
     {
