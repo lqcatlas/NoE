ab2a7df03a86862cc9e3ddf2d7e8e47ced4f5d17
lqcatlas <lqc.atlas@gmail.com>
2024-07-01 15:50:15 +0800

    remove build errors

--- Assets/Scripts/SharedWidgets/Currency Group/CurrencySet.cs
+++ Assets/Scripts/SharedWidgets/Currency Group/CurrencySet.cs
@@ -2,7 +2,6 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditorInternal;
 using UnityEngine;
 
 public class CurrencySet : MonoBehaviour
