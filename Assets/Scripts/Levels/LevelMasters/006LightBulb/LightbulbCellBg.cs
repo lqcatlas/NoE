9b7af9788a3b61d2b364c6264877095b519e5b5d
lqcatlas <lqc.atlas@gmail.com>
2024-08-10 10:49:11 +0800

    remove themes except the first twos.

--- Assets/Scripts/Levels/LevelMasters/006LightBulb/LightbulbCellBg.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class LightbulbCellBg : MonoBehaviour
-{
-    public SpriteRenderer fill;
-    public SpriteRenderer frame;
-}
